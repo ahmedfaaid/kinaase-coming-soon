17e40b6b6fc194454601fbf01a2c62f8033f4d62
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-09-29 13:15:12 +0000

    add kinaase green colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
 		extend: {
 			fontFamily: {
 				poppins: ['Poppins', 'sans-serif']
+			},
+			colors: {
+				'kinaase-green': {
+					DEFAULT: '#1c5e5c',
+					100: '#d2dfde',
+					200: '#a4bfbe',
+					300: '#779e9d',
+					400: '#497e7d',
+					500: '#1c5e5c',
+					600: '#164b4a',
+					700: '#113837',
+					800: '#0b2625',
+					900: '#061312'
+				}
 			}
 		}
 	},
